7e13ac9ddcf54cd1a7ec88ff7e394528d873e855
Ceiling_roof <ted10014@hotmail.com>
2022-05-02 12:16:07 +0800

    Committed 2022/05/02

---
+++ first_system.v
@@ -0,0 +1,15 @@
+module first_system (
+    out1, out2, in1, in2
+);
+
+// Port definitions
+input in1, in2;
+output out1, out2;
+
+// Description of the digital system
+// Dataflow modeling
+
+wire and_out, or_out;
+
+    
+endmodule
