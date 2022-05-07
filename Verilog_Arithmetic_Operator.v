ec42b957f7f62ef0ee44ea8294d7aeadae59c139
Ceiling_roof <ted10014@hotmail.com>
2022-05-08 01:08:40 +0800

    Committed 2022/05/07

---
+++ Verilog_Arithmetic_Operator.v
@@ -0,0 +1,12 @@
+// Verilog arithmetic operator
+
+module arithmetic_operation (a, b, y1, y2, y3, y4, y5);
+
+input [0:3] a, b;
+output [0:4] y1;
+output [0:5] y3;
+output [0:3] y2, y4, y5;
+
+
+
+endmodule
