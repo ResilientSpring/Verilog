5c1522e52b1123eade7f03b55e2aa73def55d8f5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-27 23:34:15 +0800

    Committed on or around 2024/03/27

---
+++ TopLevel_2.v
@@ -0,0 +1,11 @@
+module TopLevel (
+    // names for pins  
+    // (Each name can represent a single pin or a vector - group of pins)
+    SW,
+    LEDR
+);
+
+input [0:9] SW;
+output 
+    
+endmodule
