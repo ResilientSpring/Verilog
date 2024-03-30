41f372d5b32fbba13bf1f7d463dcfba94ddfc834
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-30 13:30:48 +0800

    Committed on or around 2024/03/30

--- Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing4-2.v
+++ Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing4-2.v
@@ -1,10 +1,25 @@
 module TopLevel (
     SW,
     LEDR,
-    HEX0,, HEX1, HEX2, HEX3, HEX4, HEX5
+    HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
 );
     
-    
+    input [0:5] SW;
+    output [0:5] LEDR;
+    output [0:7] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+
+    function automatic[0:7] digit;
+        input num;
+
+        begin
+            if(num == 0) digit = 8'b11000000;  // Display 0
+
+            if(num == 1) digit = 8'b11111001;  // Display 1
+
+
+        end
+        
+    endfunction
 
 
 endmodule
