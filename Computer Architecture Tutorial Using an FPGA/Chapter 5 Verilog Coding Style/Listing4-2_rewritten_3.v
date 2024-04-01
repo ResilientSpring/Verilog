caf06c3f2abda43150df73ed887b39aea13f30d4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-01 13:35:25 +0800

    Committed on or around 2024/04/01

--- Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing4-2_rewritten_3.v
+++ Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing4-2_rewritten_3.v
@@ -7,7 +7,7 @@
 module TopLevel (
     input [0:5] SW,
     output [0:5] LEDR,
-    output HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
+    output [0:7] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
 );
 
 function [0:7] digit(input state);
