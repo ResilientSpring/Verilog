fd1142b935e783c4db088b96fe9a2149670af626
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-01 13:33:34 +0800

    Committed on or around 2024/04/01

---
+++ Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing4-2_rewritten_3.v
@@ -0,0 +1,33 @@
+/* Display each switch position on both an LED and a 7-segment digit.
+   
+   If a switch is up, then the corresponding LED will be lit and 
+   the corresponding 7-segment digit will show a 1.
+*/
+
+module TopLevel (
+    input [0:5] SW,
+    output [0:5] LEDR,
+    output HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
+);
+
+function [0:7] digit(input state);
+    
+    begin
+        
+        if (state == 1) begin
+            digit = 8'b11111001;
+        end
+
+        if (state == 0) begin
+            digit = 8'b11000000;
+        end
+
+    end
+    
+endfunction
+
+assign LEDR = SW;
+
+assign HEX0 = digit(SW[0]);
+    
+endmodule
