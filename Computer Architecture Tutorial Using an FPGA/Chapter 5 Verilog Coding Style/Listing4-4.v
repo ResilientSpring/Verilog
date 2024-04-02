00520bf86874c713860668ec45be5b91854bfde1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-02 15:32:56 +0800

    Committed on or around 2024/04/02

--- Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing4-4.v
+++ Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing4-4.v
@@ -4,6 +4,20 @@ module TopLevel (
     output [7:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
 );
 
+function [7:0] digit;
+    input [1:0] num;
 
+    begin
+        if (num == 0) begin
+            digit = 8'b11000000;
+        end
+
+        if (num == 1) begin
+            digit = 8'b11111001;
+        end
+
+    end
+    
+endfunction
     
 endmodule
