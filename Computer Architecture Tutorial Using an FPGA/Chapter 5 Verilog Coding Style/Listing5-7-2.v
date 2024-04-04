f31e356abba7862e6b15d116c42f4956215274c7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-04 21:48:58 +0800

    Committed on or around 2024/04/04

--- Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing5-7-2.v
+++ Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing5-7-2.v
@@ -5,7 +5,7 @@ module TopLevel (
 
 reg openDoor;
 
-always @(SW[0] or SW[1] or SW[2]) begin
+always @(SW[0] or SW[1] or SW[2] or SW[3]) begin
     
     openDoor = (SW[0] & SW[1] & SW[2]) | SW[3];
 
