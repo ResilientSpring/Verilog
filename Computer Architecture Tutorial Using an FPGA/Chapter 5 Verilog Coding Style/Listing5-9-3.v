4a0b69393627ece56eeb119df1bdee15058476d7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-05 15:31:04 +0800

    Committed on or around 2024/04/05

--- Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing5-9-3.v
+++ Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing5-9-3.v
@@ -11,7 +11,7 @@ and(AB, notA, SW[1]);
 and(BA, notB, SW[0]);
 or(LEDR[0], AB, BA);
 
-// This is for comparison purpose. Only one statement is needed, compared to the 5 statements above.
+// This is for comparison purpose. Only 1 statement is needed, compared to 5 statements above.
 xor(LEDR[1], SW[0], SW[1]);            // Note that the output is LEDR[1].
     
 endmodule
