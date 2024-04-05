b9476b232a05013a36300f5d8f57afa14df28f65
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-05 12:53:07 +0800

    Committed on or around 2024/04/05

--- Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing5-9.v
+++ Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing5-9.v
@@ -8,6 +8,8 @@ wire  notA, notB, AB, BA;
 not(notA, SW[0]);  // ~SW[0]
 not(notB, SW[1]);  // ~SW[1]
 and(AB, notA, SW[1]);
-
+and(BA, notB, SW[0]);
+or(LEDR[0], AB, BA);
+xor(LEDR[1], SW[0], SW[1]);
     
 endmodule
