0eb7f1990528b2416c4040191100eea22a193625
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-06 12:53:27 +0800

    Committed on or around 2024/04/06

--- Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing5-11.v
+++ Computer Architecture Tutorial Using an FPGA/Chapter 5 Verilog Coding Style/Listing5-11.v
@@ -3,12 +3,18 @@ module TopLevel (
     output [1:0] LEDR
 );
 
+reg result, outcome;
 
 always @(*) begin
 
-        
+    result = LEDR[0] = ~SW[0] & SW[1] | ~SW[1] & SW[0];
+
+    outcome = SW[0] ^ SW[1];
 
 end
 
+assign LEDR[0] = result;
+
+assign LEDR[1] = outcome;
     
 endmodule
